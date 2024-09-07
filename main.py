fc368dd02ac1b834c76178c8880be9a4ca867b39
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2024-09-07 09:28:49 +0900

    [modify] Update base65536

--- main.py
+++ main.py
@@ -189,11 +189,11 @@ async def translator(request: Request, data: CharacterDataVersion1):
 
 @app.post("/rodb-simulator")
 @app.post("/rodb-simulator/{version}")
-async def rodb_simulator(request: Request, data: CharacterDataVersion1, simulator_version: int = 1):
+async def rodb_simulator(request: Request, data: CharacterDataVersion1, version: int = 1):
     data_base65536: str = ""
     try:
         # dict => json
-        data_json: str = data.to_json(sort_keys=True, indent=0)
+        data_json: str = data.to_json(indent=4)
 
         # json => bz2 copressed
         data_compressed: bytes = bz2.compress(data_json.encode("utf-8"), compresslevel=9)
@@ -210,7 +210,33 @@ async def rodb_simulator(request: Request, data: CharacterDataVersion1, simulato
     else:
         return JSONResponse({
             "success": True,
-            "url" : f"https://{request.url.hostname}/simulator/v{simulator_version}.html?{data_base65536}#main"
+            "url" : f"https://{request.url.hostname}/simulator/v{version}.html?{data_base65536}#main"
+        })
+
+@app.post("/roratorio-hub")
+@app.post("/roratorio-hub/{version}")
+async def roratorio_hub(request: Request, data: CharacterDataVersion1, version: int = 4):
+    data_base65536: str = ""
+    try:
+        # dict => json
+        data_json: str = data.to_json(indent=4)
+
+        # json => bz2 copressed
+        data_compressed: bytes = bz2.compress(data_json.encode("utf-8"), compresslevel=9)
+
+        # bz2 compressed => base65536
+        data_base65536 = base65536.encode(data_compressed)
+
+    except Exception as ex:
+        return JSONResponse({
+            "success": False,
+            "message": str(ex)
+        })
+
+    else:
+        return JSONResponse({
+            "success": True,
+            "url" : f"https://roratorio-hub.github.io/ratorio/ro{version}/m/calcx.html#{data_base65536}"
         })
 
 if __name__ == '__main__':
